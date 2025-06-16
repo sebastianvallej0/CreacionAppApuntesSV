848642272e84f073cb343b151a6fc5278567e60a
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-06-15 21:03:14 -0500

    Agrego metodos con la funcion await y filesPath

--- CreacionAppApuntesSV/Repositories/FilesRepositories.cs
+++ CreacionAppApuntesSV/Repositories/FilesRepositories.cs
@@ -10,7 +10,46 @@ namespace CreacionAppApuntesSV.Repositories
     {
         private string filePath = FileSystem.AppDataDirectory + "/archivo1.txt";
 
+        public async Task<bool> GenerarArchivo(string texto)
+        {
+            try
+            {
+                await File.WriteAllTextAsync(filePath, texto);
+                if (File.Exists(filePath))
+                {
+                    return true;
+
+                }
+                return false;
+            }
+
+            catch (Exception)
+            {
+                return false;
+
+            }
+        }
 
 
+
+        public async Task<string> DevuelveInformacionArchivo()
+        {
+            try
+            {
+
+                if (File.Exists(filePath))
+                {
+                    string texto = await File.ReadAllTextAsync(filePath);
+                    return texto;
+                }
+                return "No existe el archivo.";
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
     }
 }
