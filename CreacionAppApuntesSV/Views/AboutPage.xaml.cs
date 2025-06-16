9514d4c056bd45a94aa79a0233c9373655e8d62a
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-06-15 21:41:58 -0500

    Agrego Views y aboutpage

---
+++ CreacionAppApuntesSV/Views/AboutPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace CreacionAppApuntesSV
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
