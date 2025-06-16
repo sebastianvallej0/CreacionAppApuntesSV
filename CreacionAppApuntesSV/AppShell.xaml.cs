379063e25d87a5832ae4d1cce222946bf65e115e
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-06-15 20:44:35 -0500

    Agregar archivos de proyecto.

---
+++ CreacionAppApuntesSV/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CreacionAppApuntesSV
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CreacionAppApuntesSV/Repositories/FilesRepositories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreacionAppApuntesSV.Repositories
+{
+    internal class FilesRepositories
+    {
+        private string filePath = FileSystem.AppDataDirectory + "/archivo1.txt";
+
+
+
+    }
+}
